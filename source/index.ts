6013e82987262ef1d164d00f2561b0932ccafac6
therdas <therealrdas@gmail.com>
2020-12-03 16:59:33 +0530

    implemented time

--- source/index.ts
+++ source/index.ts
@@ -1,1 +1,14 @@
 import "./style.sass"
+initTimer(1);
+
+
+function initTimer(interval: number) {
+    setInterval(() => {
+        const d: Date = new Date();
+        let h: number = d.getHours();
+        h = h > 12 ? h - 12 : h;
+        const hr : string = h.toString().length == 1 ? "0" + h : h.toString()
+        document.querySelector('#bandDisplay > .hour')!.textContent = hr;
+        document.querySelector('#bandDisplay > .minute')!.textContent = d.getMinutes().toString();
+    }, interval * 1000);
+}
